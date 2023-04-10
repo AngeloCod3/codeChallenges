2bd58382d6f5a74c71347d6aa63f2910b622af73
Angelo de Oliveira <99901128+OliverMix@users.noreply.github.com>
2023-04-10 09:03:26 -0400

    Second Challenge

--- largestProduct/script.js
+++ largestProduct/script.js
@@ -1,4 +1,5 @@
-//ts-check
+// The four adjacent digits in the 1000-digit number that have the greatest product are 9 × 9 × 8 × 9 = 5832.
+// Find the n adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
 
 function largestProductinaSeries(n) {
   let thousandDigits = [
@@ -56,6 +57,5 @@ function largestProductinaSeries(n) {
 }
 
 largestProductinaSeries(13);
-console.time("Time");
 console.log(largestProductinaSeries(4));
-console.timeEnd("Time");
+
